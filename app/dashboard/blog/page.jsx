037037bea03237546fb433546ee96ad735a99667
aronnoahsan <aronno.ahsan.sust@gmail.com>
2023-07-31 14:57:50 +0600

    blog card added

--- app/dashboard/blog/page.jsx
+++ app/dashboard/blog/page.jsx
@@ -2,14 +2,19 @@ import BlogCard from "@/components/BlogCard";
 import styles from "@/styles/blog.module.css";
 function Blog() {
   return (
-    <div className={styles.blog__card__holder}>
-      <BlogCard title="Exploring the Mysteries of Deep Space: Unraveling the Secrets of the Cosmos" />
-      <BlogCard title="The Power of Positive Thinking: How Optimism Shapes Your Reality" />
-      <BlogCard title="From Paper to Pixels: The Evolution of Reading and its Impact on Society" />
-      <BlogCard title="The Art of Mindful Living: Embracing Serenity in a Chaotic World" />
-      <BlogCard title="Beyond the Screen: The Growing Trend of Virtual Travel and its Transformative Effects" />
-      <BlogCard title="The Science of Happiness: Unraveling the Key Ingredients to a Fulfilling Life" />
-      <BlogCard title="Revolutionizing Healthcare: The Role of Artificial Intelligence in Medicine and Patient Care" />
+    <div>
+      <div>
+        <h1 className={styles.h1__style}>Blogs</h1>
+      </div>
+      <div className={styles.blog__card__holder}>
+        <BlogCard title="Exploring the Mysteries of Deep Space: Unraveling the Secrets of the Cosmos" />
+        <BlogCard title="The Power of Positive Thinking: How Optimism Shapes Your Reality" />
+        <BlogCard title="From Paper to Pixels: The Evolution of Reading and its Impact on Society" />
+        <BlogCard title="The Art of Mindful Living: Embracing Serenity in a Chaotic World" />
+        <BlogCard title="Beyond the Screen: The Growing Trend of Virtual Travel and its Transformative Effects" />
+        <BlogCard title="The Science of Happiness: Unraveling the Key Ingredients to a Fulfilling Life" />
+        <BlogCard title="Revolutionizing Healthcare: The Role of Artificial Intelligence in Medicine and Patient Care" />
+      </div>
     </div>
   );
 }
