883cf72619ca3416e5340c0fbb6d2b57420203f0
aronnoahsan <aronno.ahsan.sust@gmail.com>
2023-07-31 11:20:33 +0600

    blog added'

--- app/dashboard/blog/page.jsx
+++ app/dashboard/blog/page.jsx
@@ -1,9 +1,15 @@
 import BlogCard from "@/components/BlogCard";
-
+import styles from "@/styles/blog.module.css";
 function Blog() {
   return (
-    <div>
-      <BlogCard />
+    <div className={styles.blog__card__holder}>
+      <BlogCard title="Exploring the Mysteries of Deep Space: Unraveling the Secrets of the Cosmos" />
+      <BlogCard title="The Power of Positive Thinking: How Optimism Shapes Your Reality" />
+      <BlogCard title="From Paper to Pixels: The Evolution of Reading and its Impact on Society" />
+      <BlogCard title="The Art of Mindful Living: Embracing Serenity in a Chaotic World" />
+      <BlogCard title="Beyond the Screen: The Growing Trend of Virtual Travel and its Transformative Effects" />
+      <BlogCard title="The Science of Happiness: Unraveling the Key Ingredients to a Fulfilling Life" />
+      <BlogCard title="Revolutionizing Healthcare: The Role of Artificial Intelligence in Medicine and Patient Care" />
     </div>
   );
 }
--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "@/components/blogcard.module.css";
 
-function BlogCard() {
+function BlogCard(props) {
+  const title = props.title;
   return (
     <div className={styles.card__holder}>
       <div>
@@ -13,19 +14,18 @@ function BlogCard() {
           className={styles.blog__thumbnail}
         />
       </div>
-      <div className={styles.date__img__flex}>
-        <Image
-          src="https://minimal-kit-react.vercel.app/assets/images/avatars/avatar_22.jpg"
-          width={40}
-          height={40}
-          alt="Author Image"
-          className={styles.author__img}
-        />
-        <div className={styles.date__style}>July 31, 2023</div>
-      </div>
-      <div className={styles.title__style}>
-        Exploring the Mysteries of Deep Space: Unraveling the Secrets of the
-        Cosmos.
+      <div className={styles.blog__data}>
+        <div className={styles.date__img__flex}>
+          <Image
+            src="https://minimal-kit-react.vercel.app/assets/images/avatars/avatar_22.jpg"
+            width={40}
+            height={40}
+            alt="Author Image"
+            className={styles.author__img}
+          />
+          <div className={styles.date__style}>July 31, 2023</div>
+        </div>
+        <div className={styles.title__style}>{title}</div>
       </div>
     </div>
   );
